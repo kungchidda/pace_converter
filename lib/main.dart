9172f6a22febb9bf7a62852f259ce2d117a7d49f
Sungjoon Hong <sjoonhong@koreafood.ca>
2024-11-06 09:35:20 -0500

    version 1

--- lib/main.dart
+++ lib/main.dart
@@ -7,46 +7,22 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pace Converter',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.transparent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Pace Converter'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +31,259 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final TextEditingController _controller1 = TextEditingController();
+  final TextEditingController _controller2 = TextEditingController();
+  final TextEditingController _minuteController3 = TextEditingController();
+  final TextEditingController _secondController3 = TextEditingController();
+  final TextEditingController _minuteController4 = TextEditingController();
+  final TextEditingController _secondController4 = TextEditingController();
 
-  void _incrementCounter() {
+  final FocusNode _focusNode1 = FocusNode();
+  final FocusNode _focusNode2 = FocusNode();
+  final FocusNode _focusNode3 = FocusNode();
+  final FocusNode _focusNode4 = FocusNode();
+  final FocusNode _focusNode5 = FocusNode();
+  final FocusNode _focusNode6 = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _focusNode1.addListener(() {
+      if (!_focusNode1.hasFocus) {
+        _calculateValues('milePerHour');
+      }
+    });
+
+    _focusNode2.addListener(() {
+      if (!_focusNode2.hasFocus) {
+        _calculateValues('kmPerHour');
+      }
+    });
+
+    _focusNode3.addListener(() {
+      if (!_focusNode3.hasFocus) {
+        _calculateValues('minutePerKm');
+      }
+    });
+
+    _focusNode4.addListener(() {
+      if (!_focusNode4.hasFocus) {
+        _calculateValues('minutePerKm');
+      }
+    });
+
+    _focusNode5.addListener(() {
+      if (!_focusNode5.hasFocus) {
+        _calculateValues('minutePerMile');
+      }
+    });
+
+    _focusNode6.addListener(() {
+      if (!_focusNode6.hasFocus) {
+        _calculateValues('minutePerMile');
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller1.dispose();
+    _controller2.dispose();
+    _minuteController3.dispose();
+    _secondController3.dispose();
+    _minuteController4.dispose();
+    _secondController4.dispose();
+
+    _focusNode1.dispose();
+    _focusNode2.dispose();
+    _focusNode3.dispose();
+    _focusNode4.dispose();
+    _focusNode5.dispose();
+    _focusNode6.dispose();
+
+    super.dispose();
+  }
+
+  void _calculateValues(String changedField) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      double milePerHour = double.tryParse(_controller1.text.isEmpty ? '0' : _controller1.text) ?? 0.0;
+      double kmPerHour = double.tryParse(_controller2.text.isEmpty ? '0' : _controller2.text) ?? 0.0;
+      double minutePerKm = (double.tryParse(_minuteController3.text.isEmpty ? '0' : _minuteController3.text) ?? 0.0) +
+          (double.tryParse(_secondController3.text.isEmpty ? '0' : _secondController3.text) ?? 0.0) / 60;
+      double minutePerMile = (double.tryParse(_minuteController4.text.isEmpty ? '0' : _minuteController4.text) ?? 0.0) +
+          (double.tryParse(_secondController4.text.isEmpty ? '0' : _secondController4.text) ?? 0.0) / 60;
+
+      switch (changedField) {
+        case 'milePerHour':
+          if (milePerHour > 0) {
+            kmPerHour = milePerHour * 1.60934;
+            minutePerKm = 60 / kmPerHour;
+            minutePerMile = 60 / milePerHour;
+          }
+          break;
+        case 'kmPerHour':
+          if (kmPerHour > 0) {
+            milePerHour = kmPerHour / 1.60934;
+            minutePerKm = 60 / kmPerHour;
+            minutePerMile = 60 / milePerHour;
+          }
+          break;
+        case 'minutePerKm':
+          if (minutePerKm > 0) {
+            kmPerHour = 60 / minutePerKm;
+            milePerHour = kmPerHour / 1.60934;
+            minutePerMile = 60 / milePerHour;
+          }
+          break;
+        case 'minutePerMile':
+          if (minutePerMile > 0) {
+            milePerHour = 60 / minutePerMile;
+            kmPerHour = milePerHour * 1.60934;
+            minutePerKm = 60 / kmPerHour;
+          }
+          break;
+      }
+
+      // Update the text fields with the calculated values
+      if (changedField != 'milePerHour') {
+        _controller1.text = milePerHour.toStringAsFixed(2);
+      }
+      if (changedField != 'kmPerHour') {
+        _controller2.text = kmPerHour.toStringAsFixed(2);
+      }
+      if (changedField != 'minutePerKm') {
+        int minutesKm = minutePerKm.floor();
+        int secondsKm = ((minutePerKm - minutesKm) * 60).round();
+        _minuteController3.text = minutesKm.toString();
+        _secondController3.text = secondsKm.toString();
+      }
+      if (changedField != 'minutePerMile') {
+        int minutesMile = minutePerMile.floor();
+        int secondsMile = ((minutePerMile - minutesMile) * 60).round();
+        _minuteController4.text = minutesMile.toString();
+        _secondController4.text = secondsMile.toString();
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 0,
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque, // Ensures the GestureDetector covers the entire area
+        onTap: () {
+          FocusScope.of(context).unfocus(); // Dismiss the keyboard
+        },
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _controller1,
+                        focusNode: _focusNode1,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Mile / Hour',
+                        ),
+                        keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _controller2,
+                        focusNode: _focusNode2,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Km / Hour',
+                        ),
+                        keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _minuteController3,
+                        focusNode: _focusNode3,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Minutes / Km',
+                        ),
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      child: TextField(
+                        controller: _secondController3,
+                        focusNode: _focusNode4,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Seconds / Km',
+                        ),
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _minuteController4,
+                        focusNode: _focusNode5,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Minutes / Mile',
+                        ),
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      child: TextField(
+                        controller: _secondController4,
+                        focusNode: _focusNode6,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Seconds / Mile',
+                        ),
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
